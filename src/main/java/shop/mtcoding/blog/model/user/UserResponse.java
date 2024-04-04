d652f1418b0e4c5494b46d3803eb01f0fac2e794
hyeon <lhyeonnit@gmail.com>
2024-04-04 11:17:48 +0900

    쌤이 말하신거 고침

--- src/main/java/shop/mtcoding/blog/model/user/UserResponse.java
+++ src/main/java/shop/mtcoding/blog/model/user/UserResponse.java
@@ -235,38 +235,10 @@ public class UserResponse {
     public static class SkillDTO {
         private Integer id;
         private String name;
-        private String color;
 
         public SkillDTO(Skill skill) {
             this.id = skill.getId();
             this.name = skill.getName();
-
-            // 혹시 언어 추가할게 있으면 else if랑 컬러, 같은 양식 맞춰서 추가가능
-            if (this.name.equals("Jquery")) {
-                this.color = "badge badge-pill bg-primary";
-            } else if (this.name.equals("JavaScript")) {
-                this.color = "badge badge-pill bg-secondary";
-            } else if (this.name.equals("Spring")) {
-                this.color = "badge badge-pill bg-success";
-            } else if (this.name.equals("HTML/CSS")) {
-                this.color = "badge badge-pill bg-danger";
-            } else if (this.name.equals("JSP")) {
-                this.color = "badge badge-pill bg-warning";
-            } else if (this.name.equals("Java")) {
-                this.color = "badge badge-pill bg-info";
-            } else if (this.name.equals("React")) {
-                this.color = "badge badge-pill bg-dark";
-            } else if (this.name.equals("Vue.js")) {
-                this.color = "badge badge-pill bg-Indigo";
-            } else if (this.name.equals("Oracle")) {
-                this.color = "badge badge-pill bg-brown";
-            } else if (this.name.equals("MySql")) {
-                this.color = "badge badge-pill bg-purple";
-            }
-            // 추가 양식
-            // else if (this.name.equals("언어")){
-            //      this.color = "badge 컬러 " ;
-
         }
     }
 
